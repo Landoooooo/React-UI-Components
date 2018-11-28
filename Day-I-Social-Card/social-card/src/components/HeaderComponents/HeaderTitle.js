612d2347590e78490d698cbc473c1c5731a297d6
Alando Appling <alandoappling@gmail.com>
2018-11-27 22:11:24 -0800

    Setup React and Finished Header Container HTML styling

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () =>{
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <span>@LambdaSchool</span>
+            <span>&#183;</span>
+            <span>26 Jan</span>
+        </div>
+    )
+}
+export default HeaderTitle;
