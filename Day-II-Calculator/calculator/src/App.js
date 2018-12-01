9afd4f0ee692c11e963c37215455e9163fe375fc
Lambda_School_Loaner_37 <lambda_school_loaner_37@Lambda-School-Loaner-37s-MacBook-Pro.local>
2018-12-01 01:01:12 -0800

    Finished calculator layout / working on functionality

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-app">
+      <CalculatorDisplay text="0" />
+      <ActionButton className="actionButtonStyle" text="Clear"/>
+      <NumberButton className="numberButtonStyle" xtraStyle="redStyle" text="&divide;"/>
+      <NumberButton className="numberButtonStyle" text="7" />
+      <NumberButton className="numberButtonStyle" text="8" />
+      <NumberButton className="numberButtonStyle" text="9" />
+      <NumberButton className="numberButtonStyle" xtraStyle="redStyle" text="&times;" />
+      <NumberButton className="numberButtonStyle" text="4" />
+      <NumberButton className="numberButtonStyle" text="5" />
+      <NumberButton className="numberButtonStyle" text="6" />
+      <NumberButton className="numberButtonStyle" xtraStyle="redStyle" text="&minus;" />
+      <NumberButton className="numberButtonStyle" text="1" />
+      <NumberButton className="numberButtonStyle" text="2" />
+      <NumberButton className="numberButtonStyle" text="3" />
+      <NumberButton className="numberButtonStyle" xtraStyle="redStyle" text="&#43;" />
+      <ActionButton className="numberButtonStyle" text="0" />
+      <NumberButton className="numberButtonStyle" xtraStyle="redStyle" text="&#61;"/>
     </div>
   );
 };
